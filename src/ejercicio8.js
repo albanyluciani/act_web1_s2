3aff9e827c75450effeb21bb92be718cf6d8daa0
Albany Luciani <albanyluciani@gmail.com>
2025-07-30 19:31:51 -0500

    ejercicio 8

--- src/ejercicio8.js
+++ src/ejercicio8.js
@@ -1,1 +1,11 @@
 //8️⃣ Transporte con tarifa reducida
+let edad = 67;
+let distanciaKm = 120;
+let tarifaBase = distanciaKm * 0.21;
+
+if (edad >= 65) {
+  tarifaBase *= 0.5;
+} else if (edad < 18) {
+  tarifaBase *= 0.8;
+}
+console.log(`Tarifa final: ${tarifaBase.toFixed(2)}`);
